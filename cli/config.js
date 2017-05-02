a287714ff06192902aebc7026fd1ada7dc25e392
Remy Sharp <remy@remysharp.com>
2017-05-02 13:54:23 +0100

    feat: quick hack to create spider command

---
+++ cli/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  commands: {
+    '_': 'lib/index',
+  },
+  options: [
+    'output',
+  ],
+  booleans: [
+    'dry-run',
+  ],
+  help: {
+    _: 'help/help.txt',
+  },
+};
---
+++ cli/index.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+const clite = require('clite');
+clite(require('./config'));
+
